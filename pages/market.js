b7dae1cecb89e6e02c4e3734e05147f51d1eecd0
adbean <adbeanx@outlook.com>
2022-06-19 14:40:01 +0800

    feat: add market page

--- pages/market.js
+++ pages/market.js
@@ -1,123 +1,247 @@
 import Image from "next/image";
 import React from "react";
 
-function store() {
+const Card = (props) => {
+  const { title, content } = props;
+
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
-      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-        <div>
-          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-            Brand new
-          </p>
+    <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl hover:scale-105 cursor-pointer">
+      <div className="p-5">
+        <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
+          <svg
+            className="w-8 h-8 text-deep-purple-accent-400"
+            stroke="currentColor"
+            viewBox="0 0 52 52"
+          >
+            <polygon
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              fill="none"
+              points="29 13 14 29 25 29 23 39 38 23 27 23"
+            />
+          </svg>
         </div>
-        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-          <span className="relative inline-block">
-            <svg
-              viewBox="0 0 52 24"
-              fill="currentColor"
-              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-            >
-              <defs>
-                <pattern
-                  id="bc9273ce-29bb-4565-959b-714607d4d027"
-                  x="0"
-                  y="0"
-                  width=".135"
-                  height=".30"
-                >
-                  <circle cx="1" cy="1" r=".7" />
-                </pattern>
-              </defs>
-              <rect fill="url(#bc9273ce-29bb-4565-959b-714607d4d027)" width="52" height="24" />
-            </svg>
-            <span className="relative">Choose</span>
-          </span>{" "}
-          your way. Advance everyday.
-        </h2>
-        <p className="text-base text-gray-700 md:text-lg">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem
-          aperiam, eaque ipsa quae.
-        </p>
+        <p className="mb-2 font-bold"> {title} </p>
+        <p className="text-sm leading-5 text-gray-900"> {content} </p>
       </div>
-      <div className="grid max-w-md gap-10 row-gap-8 lg:max-w-screen-lg sm:row-gap-10 lg:grid-cols-3 xl:max-w-screen-lg sm:mx-auto">
-        <div className="flex flex-col transition duration-300 bg-white rounded shadow-sm hover:shadow">
-          <div className="relative w-full h-48">
-            <Image
-              layout="fill"
-              src="https://images.pexels.com/photos/3184305/pexels-photo-3184305.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-              className="object-cover w-full h-full rounded-t"
-              alt="Plan"
-            />
-          </div>
-          <div className="flex flex-col justify-between flex-grow p-8 border border-t-0 rounded-b">
-            <div>
-              <div className="text-lg font-semibold">Basic</div>
-              <p className="text-sm text-gray-900">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque.
+      <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
+    </div>
+  );
+};
+
+export const Market = () => {
+  return (
+    <div className="w-full">
+      <div className="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="relative grid gap-5 sm:grid-cols-2 lg:grid-cols-4">
+          <Card
+            title={"Dijsktra"}
+            content={
+              "Sed ut perspiciatis unde omnis iste. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            }
+          />
+          <Card
+            title={"Graham"}
+            content={
+              "Sed ut perspiciatis unde omnis iste. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            }
+          />
+          <Card
+            title={"floyd"}
+            content={
+              "Sed ut perspiciatis unde omnis iste. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            }
+          />
+          <Card
+            title={"Football Sports"}
+            content={
+              "Sed ut perspiciatis unde omnis iste. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            }
+          />
+          <Card
+            title={"Football Sports"}
+            content={
+              "Sed ut perspiciatis unde omnis iste. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            }
+          />
+          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
+            <div className="p-5">
+              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-8 h-8 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <p className="mb-2 font-bold"></p>
+              <p className="text-sm leading-5 text-gray-900">
+                Disrupt inspire and think tank, social entrepreneur but preliminary thinking think
+                tank compelling.
               </p>
-              <div className="mt-1 mb-4 mr-1 text-4xl font-bold sm:text-5xl">$12</div>
             </div>
-            <a
-              href="#"
-              className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-            >
-              Buy Basic
-            </a>
+            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
           </div>
-        </div>
-        <div className="flex flex-col transition duration-300 bg-white rounded shadow-sm hover:shadow">
-          <div className="relative w-full h-48">
-            <Image
-              layout="fill"
-              src="https://images.pexels.com/photos/3183181/pexels-photo-3183181.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-              className="object-cover w-full h-full rounded-t"
-              alt="Plan"
-            />
-          </div>
-          <div className="flex flex-col justify-between flex-grow p-8 border border-t-0 rounded-b">
-            <div>
-              <div className="text-lg font-semibold">Advanced</div>
-              <p className="text-sm text-gray-900">
-                A flower in my garden, a mystery in my panties. Heart attack never was so close.
+          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
+            <div className="p-5">
+              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-8 h-8 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <p className="mb-2 font-bold">Cycling Sports</p>
+              <p className="text-sm leading-5 text-gray-900">
+                A slice of heaven. O for awesome, this chocka full cuzzie is as rip-off as a
+                cracker.
               </p>
-              <div className="mt-1 mb-4 mr-1 text-4xl font-bold sm:text-5xl">$38</div>
             </div>
-            <a
-              href="#"
-              className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-            >
-              Buy Advanced
-            </a>
+            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
           </div>
-        </div>
-        <div className="flex flex-col transition duration-300 bg-white rounded shadow-sm hover:shadow">
-          <div className="relative w-full h-48">
-            <Image
-              layout="fill"
-              src="https://images.pexels.com/photos/3182743/pexels-photo-3182743.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-              className="object-cover w-full h-full rounded-t"
-              alt="Plan"
-            />
-          </div>
-          <div className="flex flex-col justify-between flex-grow p-8 border border-t-0 rounded-b">
-            <div>
-              <div className="text-lg font-semibold">Pro</div>
-              <p className="text-sm text-gray-900">
-                We never had the chance to. Maybe it was the eleven months he spent in the womb.
+          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
+            <div className="p-5">
+              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-8 h-8 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <p className="mb-2 font-bold">Weight Lifting Sports</p>
+              <p className="text-sm leading-5 text-gray-900">
+                Meanwhile, in behind the bicycle shed, Hercules Morse, as big as a horse.
               </p>
-              <div className="mt-1 mb-4 mr-1 text-4xl font-bold sm:text-5xl">$78</div>
             </div>
-            <a
-              href="#"
-              className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-            >
-              Buy Pro
-            </a>
+            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
+          </div>
+          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
+            <div className="p-5">
+              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-8 h-8 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <p className="mb-2 font-bold">Golf Sports</p>
+              <p className="text-sm leading-5 text-gray-900">
+                Disrupt inspire and think tank, social entrepreneur but preliminary thinking think
+                tank compelling.
+              </p>
+            </div>
+            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
+          </div>
+          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
+            <div className="p-5">
+              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-8 h-8 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <p className="mb-2 font-bold">Hockey Sports</p>
+              <p className="text-sm leading-5 text-gray-900">
+                A business big enough that it could be listed on the NASDAQ goes belly up.
+              </p>
+            </div>
+            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
+          </div>
+          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
+            <div className="p-5">
+              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-8 h-8 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <p className="mb-2 font-bold">Shooting Sports</p>
+              <p className="text-sm leading-5 text-gray-900">
+                Lookout flogging bilge rat main sheet bilge water nipper fluke to go on account
+                heave down clap of thunder.
+              </p>
+            </div>
+            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
+          </div>
+          <div className="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
+            <div className="p-5">
+              <div className="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
+                <svg
+                  className="w-8 h-8 text-deep-purple-accent-400"
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+              <p className="mb-2 font-bold">Martial Arts</p>
+              <p className="text-sm leading-5 text-gray-900">
+                Webtwo ipsum orkut reddit meebo skype vimeo jajah spock empressr zimbra, mobly
+                napster.
+              </p>
+            </div>
+            <div className="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default store;
+export default Market;
