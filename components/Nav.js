c5a866b227fb878fe41ea7dc6171a77ef3eaf6a6
adbean <adbeanx@outlook.com>
2022-08-02 01:16:15 +0800

    feat: drop down

--- components/Nav.js
+++ components/Nav.js
@@ -4,10 +4,19 @@ import Image from "next/image";
 import SnackBar from "@components/SnackBar";
 import { useEffect, useState } from "react";
 import { getUserInfo } from "@api/api";
+import { useRouter } from "next/router";
 
 export const Nav = (props) => {
+  const router = useRouter();
   const { userId, setUserId, message, setMessage } = props;
   const [info, setInfo] = useState(null);
+
+  const userSettings = [
+    { label: "用户名" },
+    { label: "邮箱" },
+    { label: "设置", href: "" },
+    { label: "退出", href: "/" },
+  ];
 
   useEffect(() => {
     const getInfo = async () => {
@@ -78,16 +87,53 @@ export const Nav = (props) => {
               </li>
               <li>
                 {userId ? (
-                  <div className="relative flex-shrink-0">
-                    <Image
-                      src={info?.avatar || "https://source.unsplash.com/50x50/?portrait"}
-                      alt="Avatar"
-                      onClick={() => logout()}
-                      width={32}
-                      height={32}
-                      className="hover:cursor-pointer w-12 h-12 border rounded-full dark:bg-gray-500 dark:border-gray-700"
-                    />
-                  </div>
+                  <>
+                    <div className="relative flex-shrink-0 peer group">
+                      <Image
+                        src={info?.avatar || "https://source.unsplash.com/50x50/?portrait"}
+                        alt="Avatar"
+                        onClick={() => logout()}
+                        width={32}
+                        height={32}
+                        className="pb-10 hover:cursor-pointer w-12 h-12 border rounded-full dark:bg-gray-500 dark:border-gray-700"
+                      />
+                      <ul className="invisible peer-hover:visible group-hover:visible transition-all ease-in-out absolute top-11 z-50 shadow-indigo-500/40 -left-12 text-base text-[#838c96] rounded-sm antialiased border-[#e7ebf0] bg-white border">
+                        {userSettings.map((setting, idx) => (
+                          <li
+                            key={idx}
+                            className="text-center whitespace-nowrap tracking-wider my-1 py-2 leading-none px-4 cursor-pointer hover:bg-[#f4f6fb] hover:text-[#40454d] text-sm"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              if (setting.label === "设置") {
+                                router.push("/settings");
+                              } else if (setting.label === "退出") {
+                                logout();
+                              }
+                            }}
+                          >
+                            {setting.label}
+
+                            {setting.label === "用户名" && (
+                              <>
+                                <br />
+                                <span className="text-xs text-[10px] cursor-default">
+                                  {info?.username || "username"}
+                                </span>
+                              </>
+                            )}
+                            {setting.label === "邮箱" && (
+                              <>
+                                <br />
+                                <span className="text-xs text-[10px] cursor-default">
+                                  {info?.email || "email@email.com"}
+                                </span>
+                              </>
+                            )}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </>
                 ) : (
                   <Link href="/signin">
                     <a
